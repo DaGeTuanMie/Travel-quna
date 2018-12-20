dd8362e74133c793eb3755b009dc4e505efcd258
646882070@qq.com <646882070@qq.com>
2018-12-20 14:57:22 +0800

    travel init

---
+++ src/packages/home/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="header">
+      <div class="header-left"><div class="iconfont icon1">&#xe696;</div></div>
+      <div class="header-input"><i class="iconfont icon2">&#xe60b;</i>输入城市/游玩/主题</div>
+      <div class="header-right">城市<i class="iconfont icon3">&#xe60c;</i></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeHeader"
+    }
+</script>
+
+<style scoped lang="stylus">
+  @import "~@/assets/styles/varibles.styl"
+  .header
+    display flex
+    line-height .86rem
+    color #ffffff
+    background $bgColor
+    .header-left
+      width .64rem
+      float left
+      .icon1
+        font-size .4rem
+        text-align center
+    .header-input
+      flex 1
+      height .64rem
+      line-height .64rem
+      padding-left .2rem
+      margin-top .12rem
+      margin-left .2rem
+      background #ffffff
+      color #ccc
+      border-radius .1rem
+    .header-right
+      width 1.24rem
+      float right
+      text-align center
+      .icon3
+        vertical-align top
+        font-size .24rem
+</style>
---
+++ src/packages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="swiper">
+      <swiper :options="swiperOption" ref="mySwiper">
+        <!-- slides -->
+        <swiper-slide v-for="item of swiperList" :key=item.id>
+          <img class="swiper-img" :src="item.imgUrl">
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeSwiper",
+        data(){
+          return{
+            swiperOption:{
+              pagination:'.swiper-pagination',
+              loop:true
+            },
+            swiperList:[{
+              id:'0001',
+              imgUrl:"http://img1.qunarzz.com/piao/fusion/1812/a8/33a03fe1c213da02.jpg_750x200_dd491fc5.jpg"
+            },{
+              id:'0002',
+              imgUrl:"http://img1.qunarzz.com/piao/fusion/1812/92/edb99066ecb8ac02.jpg_750x200_059678c3.jpg"
+        }]
+          }
+        }
+    }
+</script>
+
+<style scoped lang="stylus">
+  .swiper >>> .swiper-pagination-bullet-active
+    background #ffffff
+  .swiper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 26.7%
+    .swiper-img
+      width 100%
+</style>
